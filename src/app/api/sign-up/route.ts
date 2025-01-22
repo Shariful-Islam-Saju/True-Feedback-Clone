5780d5553e9662a8521fec1b420371494a4aaf3f
Shariful Islam <sajukhan12905@gmail.com>
2025-01-22 23:33:57 +0600

    time 1:50

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -5,9 +5,40 @@ import bcrypt from "bcryptjs";
 
 export async function POST(req: Request) {
   await dbConnect();
+  const { username, email, password } = await req.json();
+  const isUserVerified = await UserModel.findOne({
+    username,
+    isVerified: true,
+  });
+  if (isUserVerified) {
+    return Response.json(
+      { success: false, message: "User already exists" },
+      {
+        status: 400,
+      }
+    );
+  }
+  const isExistingUser = await UserModel.findOne({ email });
+  const verificationCode = Math.floor(100000 + Math.random() * 900000);
+  if (isExistingUser) {
+    //todo: send verification email
+  } else {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const expiryDate = new Date();
+    expiryDate.setHours(expiryDate.getHours() + 1);
+    const user = new UserModel({
+      username,
+      email,
+      password: hashedPassword,
+      verifyCode: verificationCode,
+      verifyCodeExpiry: expiryDate,
+      isAcceptingMessages: true,
+      messages: [], 
+    });
+    await user.save();
+  }
 
   try {
-    
   } catch (error) {
     console.log("Error in sign-up route", error);
     Response.json(
